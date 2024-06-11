9261988b90d023c0c5c30b0907125ee7733ace08
bananaMatcha <146593728+bananaMatcha@users.noreply.github.com>
2024-06-11 13:10:08 +1000

    Add files via upload

---
+++ 2.2P Counter/Counter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter
+{
+    public class Counter
+    {
+        private string _name;
+        private int _count;
+
+        public string Name //property
+        {
+            get 
+            { 
+                return _name; 
+            }
+            set 
+            {
+                _name = value; 
+            }
+        }
+
+        public int Ticks
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public Counter(string name)
+        {
+            _name = name;
+            _count = 0;
+        }
+
+        public void Increment()
+        {
+            _count++;
+        }
+
+        public void Reset()
+        {
+             _count = 0;
+        }
+
+       
+
+    }
+}
---
+++ 2.2P Counter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Counter
+{
+    internal class Program
+    {
+        static private void PrintCounters(Counter[] counters) //take parameter as an array of the 'Counter' Class 
+        {
+            foreach (Counter counter in counters) // foreach (Counter myCounters[i] in myCounters) 
+            {
+                Console.WriteLine("{0} is {1}", counter.Name, counter.Ticks); //access Counter's properties
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Counter[] myCounters = new Counter[3]; // declare an array with 3 items named 'myCounters'
+
+            myCounters[0] = new Counter("Counter 1"); //declare Counter object has a name of "Counter 1"
+            myCounters[1] = new Counter("Counter 2");
+            myCounters[2] = myCounters[0];
+
+            for (int i = 1; i <= 9; i++)
+            {
+                myCounters[0].Increment(); 
+            }
+
+            for (int i = 1; i <= 14; i++)
+            {
+                myCounters[1].Increment();
+            }
+
+            PrintCounters(myCounters);
+            myCounters[2].Reset(); //call Reset method from the Counter class
+
+            PrintCounters(myCounters);
+
+            Console.WriteLine("Press any key to exit..."); 
+            Console.ReadKey(); // Wait for user input to exit
+        }
+    }
+}
